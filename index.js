d4c41affca950120df582e945dc86860f17209b3
Mohnish <shethmohnish@gmail.com>
2023-12-07 22:14:18 -0500

    Added database functionality through MongoDB

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ import express from 'express';
 import path from 'path';
 import ejs from 'ejs';
 import bodyParser from 'body-parser';
+import {MongoClient, ServerApiVersion} from 'mongodb';
 
+const uri = "mongodb+srv://Admin:nR18eHCkif6yvno0@cluster0.ak6hid0.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
 const app = express();
 
 app.use(express.static('public'));
@@ -35,16 +39,48 @@ app.get('/contact', (req, res) => {
 
 app.get('/sign-up', (req, res) => {
     res.render('sign-up');
-})
+});
 
 app.get('/log-in', (req, res) => {
     res.render('log-in');
-})
+});
 
 app.get('/profile', (req, res) => {
     res.render('profile');
-})
+});
+
+app.post('/submit-form', async (req, res) => {
+  try {
+    const formData = req.body;
+    console.log(formData);
+
+    await saveDetails(formData);
+    
+    res.redirect('/');
+  } 
+  catch (error) {
+    console.error(error);
+    res.send('Error submitting details, try again.');
+  }
+});
 
 app.listen(3000, () => {
     console.log('Express server initialized');
 });
+
+//Saving the user details to the database by connecting to it and inserting the form details.
+async function saveDetails(formData) {
+  try {
+
+    await client.connect();
+    const database = client.db("user-details");
+    const details = database.collection("details");
+
+    const result = await details.insertOne(formData);
+
+    console.log("User details sumbitted");
+
+  } finally {
+    await client.close();
+  }
+}
