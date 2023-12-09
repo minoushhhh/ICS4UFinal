e78b27b2ab91a2f15bcbc9a7691725a2f1c459ce
Mohnish <shethmohnish@gmail.com>
2023-12-08 21:08:47 -0500

    Updated login handling.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'path';
 import ejs from 'ejs';
 import bodyParser from 'body-parser';
-import {MongoClient, ServerApiVersion} from 'mongodb';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 const uri = "mongodb+srv://Admin:nR18eHCkif6yvno0@cluster0.ak6hid0.mongodb.net/?retryWrites=true&w=majority";
 
@@ -18,51 +18,92 @@ app.set('view engine', 'ejs');
 const __dirname = path.resolve();
 
 app.get('/', (req, res) => {
-    res.render('index');
+  res.render('index');
 });
 
 app.get('/index', (req, res) => {
-    res.render('index');
+  res.render('index');
 });
 
 app.get('/booking', (req, res) => {
-    res.render('booking');
+  res.render('booking');
 });
 
 app.get('/estimate', (req, res) => {
-    res.render('estimate');
+  res.render('estimate');
 });
 
 app.get('/contact', (req, res) => {
-    res.render('contact');
+  res.render('contact');
 });
 
 app.get('/sign-up', (req, res) => {
-    res.render('sign-up');
+  res.render('sign-up');
 });
 
 app.get('/login', (req, res) => {
-    res.render('login');
+  res.render('login');
 });
 
 app.get('/profile', (req, res) => {
-    res.render('profile');
+  res.render('profile');
 });
 
 app.post('/login-form', async (req, res) => {
   try {
-    const username = req.body.username + "";
-    const password = req.body.password + "";
-    console.log(username + " " + password); 
+    const user = req.body.username + "";
+    const pass = req.body.password + "";
 
-    await checkLogin(username, password);
+    console.log(user + " " + pass);
 
-    console.log("Sucessfully logged in!")
-    res.redirect("/");
+    try {
+      await client.connect();
+
+      const database = client.db("user-details");
+      const details = database.collection("details");
+
+      let query;
+
+      if (user.includes("@")) {
+        const email = { email: user };
+        query = await details.findOne(email);
+        if (query === null) {
+          res.send("User or Email not found");
+          return;
+        }
+      }
+      else {
+        const username = { username: user };
+        query = await details.findOne(username);
+        if (query === null) {
+          res.send("User or Email not found");
+          return;
+        }
+      }
+
+      const detailsDoc = await details.findOne(query)
+
+      if (detailsDoc) {
+        const password = detailsDoc.password;
+        if (password == pass) {
+          console.log("Correct password");
+          res.redirect("/");
+        }
+        else {
+          console.log("Incorrect password");
+          res.send("Incorrect password.");
+          return;
+        }
+      }
+    }
+    finally {
+      await client.close();
+    }
   }
   catch (error) {
     console.error(error);
-    res.send('Error logging in, try again.');
+    res.send("Login failed, try again.");
+    return;
   }
 });
 
@@ -72,9 +113,9 @@ app.post('/submit-form', async (req, res) => {
     console.log(formData);
 
     await saveDetails(formData);
-    
+
     res.redirect('/');
-  } 
+  }
   catch (error) {
     console.error(error);
     res.send('Error submitting details, try again.');
@@ -82,11 +123,11 @@ app.post('/submit-form', async (req, res) => {
 });
 
 app.get('/test', (req, res) => {
-    res.render('test');
+  res.render('test');
 })
 
 app.listen(3000, () => {
-    console.log('Express server initialized');
+  console.log('Express server initialized');
 });
 
 //Saving the user details to the database by connecting to it and inserting the form details.
@@ -102,42 +143,6 @@ async function saveDetails(formData) {
     console.log("User details sumbitted");
 
   } finally {
-    await client.close();
-  }
-}
-
-async function checkLogin(user, pass) {
-  try {
-    await client.connect();
-
-    const database = client.db("user-details");
-    const details = database.collection("details");
-
-    let query;
-
-    if (user.includes("@")) {
-      const email = {email: user};
-      query = await details.findOne(email);
-    }
-    else {
-      const username = {username: user};
-      query = await details.findOne(username);
-    }
-
-    const document = await details.findOne(query)
-
-    if (document) {
-      const password = document.password;
-      if (password == pass) {
-        console.log("Correct password")
-      }
-      else {
-        console.log("Incorrect password");
-      }
-    }
-
-  }
-  finally {
     await client.close();
   }
 }
